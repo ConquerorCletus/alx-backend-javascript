61043e7fe153b59400d9ab290906d28a3b3e2750
ConquerorCletus <smartchibuike2019@gmail.com>
2023-11-30 21:53:54 +0100

    ES6 classes

---
+++ 0x02-ES6_classes/main/0-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-unresolved */
+import ClassRoom from '../0-classroom';
+
+const room = new ClassRoom(10);
+// eslint-disable-next-line no-underscore-dangle
+console.log(room._maxStudentsSize);
