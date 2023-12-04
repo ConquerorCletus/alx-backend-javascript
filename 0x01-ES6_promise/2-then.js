ad2d2451f27ef2ac7f6a314b458b14be45f293d1
ConquerorCletus <smartchibuike2019@gmail.com>
2023-12-04 05:43:13 +0100

    another implementation of 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(Error());
-    }
-  });
-  return myPromise.finally(console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
